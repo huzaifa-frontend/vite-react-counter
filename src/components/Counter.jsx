22aa4de723a370d921da3f6414e827e1979154fe
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-01 17:38:44 +0500

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div>
-      <p>Counter Goes Here</p>
+    <div style={{ textAlign: "center", marginTop: "30px" }}>
+      <h2>Count: {count}</h2>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
