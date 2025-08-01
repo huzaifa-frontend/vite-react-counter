4d5db5c5fa940f5804853139f468a93a94f23743
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-01 17:12:08 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Counter from './components/Counter'
+import Counter from "./components/Counter";
 
 function App() {
   reutrn(
     <div>
-      <h1 style={{textAlign: 'center'}}>React Counter App</h1>
+      <h1 style={{ textAlign: "center" }}>React Counter App</h1>
     </div>
   );
 }
