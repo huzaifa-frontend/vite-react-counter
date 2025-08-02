4dc5b6f72e043bd3015185adf98696b31b3a9731
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-08-02 16:20:46 +0500

    Removed h1 heading from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>React Counter App</h1>
       <Counter />
     </div>
   );
